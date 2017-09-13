848f8168eda34df9d76ab6c4489cc6732ad01ebf
Paul Lin <hclin.welcome@gmail.com>
2017-09-14 00:18:11 +0800

    fix bug; add meta_dict

--- src/dler/dler/dler.py
+++ src/dler/dler/dler.py
@@ -15,6 +15,7 @@ CURL_OPT_USER_AGENT_LIST = [ 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (K
 LEN_USER_AGENT_LIST = len(CURL_OPT_USER_AGENT_LIST)
 KEY_CURL_HEADER_RESPONSE = '_response'
 KEY_CURL_HEADER_LOCATION = 'location'
+KEY_META_REDIRECT_PATH = 'redirect_url'
 
 ''' TODO
 1. make a singleton
@@ -24,13 +25,14 @@ KEY_CURL_HEADER_LOCATION = 'location'
 
 class DlerError(Exception): pass
 class Dler(object):
-    def __init__(self, max_thread = 5, cache = None, header_cache = None):
+    def __init__(self, max_thread = 5, cache = None, header_cache = None, meta_cache = None):
         self.max_thread = max_thread
         self.thread_pool = {}
         self.condition = threading.Condition()
         self.event = threading.Event()
         self.cache = {} if cache is None else cache
         self.header_cache = {} if header_cache is None else header_cache
+        self.meta_cache = {} if meta_cache is None else meta_cache
     
     def download(self, url_iterable):
         try:
@@ -56,17 +58,21 @@ class Dler(object):
         quota = self.max_thread - len_thread_pool
         for i in xrange(min(quota, len_url_set)):
             url = url_set.pop()
-            self.thread_pool[url] = DlerThread(url, self.condition, self.event, self.thread_pool, self.cache, self.header_cache, user_agent_num)
+            self.cache[url] = {}
+            self.header_cache[url] = {}
+            self.meta_cache[url] = {}
+            self.thread_pool[url] = DlerThread(url, self.condition, self.event, self.thread_pool, self.cache[url], self.header_cache[url], self.meta_cache[url], user_agent_num)
             self.thread_pool[url].start()
-        
 
 
 class DlerThread(threading.Thread):
-    def __init__(self, url, condition, event, thread_pool, content_dict, header_dict, user_agent_num):
+    def __init__(self, url, condition, event, thread_pool, content_dict, header_dict, meta_dict, user_agent_num):
         threading.Thread.__init__(self)
         self.content_dict = content_dict
         self.header_dict = header_dict
+        self.meta_dict = meta_dict
         self.url = url
+        self.url_chain = [url]
         self.con = condition
         self.event  = event
         self.thread_pool = thread_pool
@@ -75,8 +81,6 @@ class DlerThread(threading.Thread):
     def run(self):
         self.download_url()
         self.thread_pool.pop(self.url, None)
-       
-        return self.content_dict, self.header_dict
 
     def download_url(self):
         download_chain = [self.url]
@@ -87,6 +91,8 @@ class DlerThread(threading.Thread):
             if KEY_CURL_HEADER_LOCATION in self.header_dict[url] and http_code >= 300 and http_code < 400:
                 next_url = self._compose_url_from_location(url, self.header_dict[url][KEY_CURL_HEADER_LOCATION])
                 download_chain.append(next_url)
+                self.url_chain.append(next_url)
+        self.meta_dict[KEY_META_REDIRECT_PATH] = self.url_chain
 
     def _compose_url_from_location(self, url, location_url):
         if location_url.startswith('http'):
@@ -94,7 +100,6 @@ class DlerThread(threading.Thread):
 
         url_tok = urlparse(url)
         return '%s://%s%s'%(url_tok.scheme, url_tok.netloc, location_url)
-        
 
     def _curl(self, url, user_agent_num=0):
         string_buffer = StringIO()
@@ -129,7 +134,6 @@ class DlerThread(threading.Thread):
             _tmp[name] = value
 
         return _tmp
-            
         
 
 if __name__ == '__main__':
@@ -140,3 +144,5 @@ if __name__ == '__main__':
     #print dler.cache
     #print dler.header_cache
     print time.time()
+    print dler.meta_cache['http://google.com'][KEY_META_REDIRECT_PATH]
+    print dler.meta_cache['http://paypal.com'][KEY_META_REDIRECT_PATH]
