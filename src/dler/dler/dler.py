ac0aa968d8662d668496a515731cc5f9addc7db4
Paul Lin <hclin.welcome@gmail.com>
2017-09-18 02:35:27 +0800

    add re for content parsing; add header for accept lang

--- src/dler/dler/dler.py
+++ src/dler/dler/dler.py
@@ -4,6 +4,7 @@ import time
 from StringIO import StringIO
 import random
 from urlparse import urljoin
+import re
 
 
 CURL_OPT_MAX_NUM_REDIRECT = 12
@@ -22,12 +23,13 @@ KEY_META_SUCCESSFULLY_DOWNLOAD = 'successfully_download'
 KEY_META_FAILED_DOWNLOAD_REASON = 'failed_download_reason'
 KEY_META_HTTP_CODE = 'http_code'
 
+RE_HEAD = re.compile('<\s*head\s+[^>]*>(.*)</head>')
+RE_JAVASCRIPT = re.compile('<\s*script\s*>(.*)?<\s*script\s*>')
+RE_META_REDIRECT = re.compile('<\s*meta\s+[^>]*http-equiv\s*=\s*[\'"]refresh[\'"]\s*[^>]*content\s*=\s*[\'"][^\'"]*;\s*url\s*=\s*([^\'"]*)[\'"]')
+RE_WINDOW_LOCATION_REDIRECT = re.compile('window.location=[\'"]([^\'"]+)[\'"]')
+RE_DOCUMENT_LOCATION_HREF_REDIRECT = re.compile('document.location.href=[\'"](^\'"]+)[\'"]')
+RE_FORM_SUBMIT_REDIRECT = re.compile('document.forms\[[0-9]+\].submit()')
 
-''' TODO
-1. make a singleton
-2. add timeout to pycurl opt 
-3. remove __main__
-'''
 
 class DlerError(Exception): pass
 class Dler(object):
@@ -40,27 +42,27 @@ class Dler(object):
         self.header_cache = {} if header_cache is None else header_cache
         self.meta_cache = {} if meta_cache is None else meta_cache
     
-    def download(self, url_iterable):
+    def download(self, url_iterable, does_content_redirect=False):
         try:
             url_iterator = iter(url_iterable)
         except TypeError as e:
             raise DlerError(e)
 
         url_set = set(url_iterator)
-        user_agent_num = random.randint(0,LEN_USER_AGENT_LIST-1)
+        user_agent_num = random.randint(0, LEN_USER_AGENT_LIST-1)
 
         while len(self.thread_pool) != 0 or len(url_set) != 0 :
             _len_thread_pool = len(self.thread_pool)
             _len_url_set = len(url_set)
 
             if _len_thread_pool < self.max_thread and _len_url_set > 0:
-                self._parallel_download(url_set, _len_thread_pool, _len_url_set, user_agent_num)
+                self._parallel_download(url_set, _len_thread_pool, _len_url_set, user_agent_num, does_content_redirect)
 
             time.sleep(0.1) 
 
         return self
 
-    def _parallel_download(self, url_set, len_thread_pool, len_url_set, user_agent_num=0):
+    def _parallel_download(self, url_set, len_thread_pool, len_url_set, user_agent_num, does_content_redirect):
         quota = self.max_thread - len_thread_pool
         for i in xrange(min(quota, len_url_set)):
             url = url_set.pop()
@@ -68,16 +70,17 @@ class Dler(object):
             self.header_cache[url] = {}
             self.meta_cache[url] = {}
             try:
-                self.thread_pool[url] = DlerThread(url, self.condition, self.event, self.thread_pool, self.cache[url], self.header_cache[url], self.meta_cache[url], user_agent_num)
+                self.thread_pool[url] = DlerThread(url, self.condition, self.event, self.thread_pool, self.cache[url], self.header_cache[url], self.meta_cache[url], user_agent_num, does_content_redirect)
                 self.thread_pool[url].start()
             except DlerThreadError as e:
                 self.meta_cache[url][KEY_META_SUCCESSFULLY_DOWNLOAD] = False
                 self.meta_cache[url][KEY_META_FAILED_DOWNLOAD_REASON] = '%s:%s' % (e.__class__.__name__, str(e))
                 continue
 
+
 class DlerThreadError(Exception): pass
 class DlerThread(threading.Thread):
-    def __init__(self, url, condition, event, thread_pool, content_dict, header_dict, meta_dict, user_agent_num):
+    def __init__(self, url, condition, event, thread_pool, content_dict, header_dict, meta_dict, user_agent_num, does_content_redirect, max_redirect = None):
         threading.Thread.__init__(self)
         self.content_dict = content_dict
         self.header_dict = header_dict
@@ -88,28 +91,51 @@ class DlerThread(threading.Thread):
         self.event  = event
         self.thread_pool = thread_pool
         self.user_agent_num = user_agent_num
+        self.max_redirect = 12 if max_redirect is None else max_redirect
+        self.does_content_redirect = does_content_redirect
+        ''' TODO accept langauge will be used to target regional, e.g. only open for zh-cn and block if there is ja-jp '''
+        self.custom_header = ['Accept-Language:zh-tw,zh-cn,zh-hk,zh-mo,en-us,en-gb,en-ca,fr-fr,de-de,it-it,ja-jp,ru-ru,es-es,pt-br,es-mx,bn-in,da-dk']
 
     def run(self):
         self.download_url()
         self.thread_pool.pop(self.url, None)
 
-    def download_url(self, max_redirect = None):
+    def download_url(self):
         download_chain = [self.url]
         redirected_num = 0
 
         while(len(download_chain) != 0):
+            do_redirect = False
+
             url = download_chain.pop(0)
             self.content_dict[url], self.header_dict[url], http_code = self._curl(url, self.user_agent_num)
 
             if KEY_CURL_HEADER_LOCATION in self.header_dict[url] and http_code >= 300 and http_code < 400:
                 next_url = urljoin(url, self.header_dict[url][KEY_CURL_HEADER_LOCATION])
                 if not next_url or url == next_url: continue
+                do_redirect = True
 
+            ''' 
+            This function aggressively find redirect URL
+            by parse content and find the possible auto triggerring connect URL.
+            Lower the content for regular expression match, but it might wrongly update URL and cause 404
+            ''' 
+            if not do_redirect and self.does_content_redirect:
+                url_set = find_redirect(self.content_dict[url].lower())
+                len_url_set = len(url_set)
+
+                if len_url_set == 1: 
+                    next_url = urljoin(url, url_set.pop())
+                    do_redirect = True
+                elif len_url_set > 1:
+                    raise DlerThreadError('Get more than 1 URL to redirect from content parsing')
+
+            if do_redirect:
                 download_chain.append(next_url)
                 self.url_chain.append(next_url)
                 redirected_num += 1
 
-                if max_redirect is not None and redirect_num >= max_redirect:
+                if redirected_num >= self.max_redirect:
                     self.meta_dict[KEY_META_IS_REDIRECT_COMPLETE] = False
                     break
 
@@ -128,6 +154,7 @@ class DlerThread(threading.Thread):
         header_buffer = list()
         c = pycurl.Curl()
         c.setopt(c.URL, url)
+        c.setopt(c.HTTPHEADER, self.custom_header)
         c.setopt(c.WRITEFUNCTION, string_buffer.write)
         c.setopt(c.FOLLOWLOCATION, False)
         c.setopt(c.USERAGENT, CURL_OPT_USER_AGENT_LIST[user_agent_num])
@@ -164,15 +191,35 @@ class DlerThread(threading.Thread):
             _tmp[name] = value
 
         return _tmp
-        
+
+
+def find_redirect(lower_page):
+    redirect_url = set()
+
+    heads = RE_HEAD.findall(lower_page)
+    for item in heads:
+        for url in RE_META_REDIRECT.findall(item): redirect_url.add(url)
+
+    scripts = RE_JAVASCRIPT.findall(lower_page)
+    for item in scripts:
+        for url in RE_WINDOW_LOCATION_REDIRECT.findall(item): redirect_url.add(url)
+        for url in RE_DOCUMENT_LOCATION_HREF_REDIRECT.findall(item): redirect_url.add(url)
+        for url in RE_FORM_SUBMIT_REDIRECT.findall(item): redirect_url.add(url)
+
+    return set([url.strip() for url in redirect_url if url.strip()])
+            
 
 if __name__ == '__main__':
     dler = Dler()
     #dler.download(['1', '2', '3', '4', '5'])
     print time.time()
-    dler.download(['https://facebook.com', 'http://google.com', 'http://github.com', 'http://twitch.tv', 'http://paypal.com'])
+    #dler.download(['https://facebook.com', 'http://google.com', 'http://github.com', 'http://twitch.tv', 'http://paypal.com'])
     #print dler.cache
     #print dler.header_cache
-    print time.time()
-    print dler.meta_cache['http://google.com'][KEY_META_REDIRECT_PATH]
-    print dler.meta_cache['http://paypal.com'][KEY_META_REDIRECT_PATH]
+    #print time.time()
+    #print dler.meta_cache['http://google.com'][KEY_META_REDIRECT_PATH]
+    #print dler.meta_cache['http://paypal.com'][KEY_META_REDIRECT_PATH]
+
+    import sys
+    dler.download([sys.argv[1]], True)
+    print dler.meta_cache
