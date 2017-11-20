2383cb5d4f429202576b131430be9c7d62ee9e94
Paul Lin <hclin.welcome@gmail.com>
2017-11-20 22:41:38 +0800

    use lxml

--- src/extractor/extractor/extractor.py
+++ src/extractor/extractor/extractor.py
@@ -205,7 +205,7 @@ def tag_visible(element):
     return True
 
 def text_from_html(body):
-    soup = BeautifulSoup(body, 'html.parser')
+    soup = BeautifulSoup(body, 'lxml')
     texts = soup.findAll(text=True)
     visible_texts = filter(tag_visible, texts)  
     return u" ".join(t.strip() for t in visible_texts)
