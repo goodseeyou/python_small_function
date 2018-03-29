333af1115735176ce524421c110f7667060bb4b7
Paul Lin <hclin.welcome@gmail.com>
2018-03-29 13:47:32 +0800

    update func

--- src/extractor/extractor/extractor.py
+++ src/extractor/extractor/extractor.py
@@ -107,10 +107,10 @@ class Extractor(object):
         else:
             return [tok.lower().strip() for tok in RE_ENG_NUM_TEXT.findall(text)]
 
-    def does_have_long_document_write_unescape(self):
+    def does_have_long_document_write_unescape(self, threshold=15):
         result = RE_WRITE_UNESCAPE.search(self.page_lower)
         txt = result.group(0) if result else ''
-        return result is not None and txt.count('%') > 10
+        return result is not None and txt.count('%') > threshold
 
 
     def _get_href_from_tag(self, tags):
