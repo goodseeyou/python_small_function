4f0d309008c7e242a84f5430abce7d1896df4f0c
Paul Lin <hclin.welcome@gmail.com>
2018-08-15 11:12:18 +0800

    update

--- src/urlTool/urlTool.py
+++ src/urlTool/urlTool.py
@@ -6,42 +6,52 @@ TRANS_NUMBER_TO_ZERO = maketrans(''.join((str(i) for i in range(10))), ''.join((
 PREFIX_LENGTH = 8
 
 
-class UrlModuleError(Exception): pass
+class UrlModuleError(Exception):
+    pass
+
+
 class UrlModule(object):
-    _url_tok = None
+    _url_attr = None
     _query_dict = None
 
     def __init__(self, url):
-        self.url = url
+        self.url = url.strip()
 
     @property
-    def url_tok(self):
-        if self._url_tok is None:
+    def url_attr(self):
+        if self._url_attr is None:
             try:
-                self._url_tok = urlparse(self.url)
+                self._url_attr = urlparse(self.url)
             except ValueError as e:
-                raise UrlModuleError(e)
+                raise UrlModuleError('Faield to parse url %s' % self.url, e)
 
-        return self._url_tok
+        return self._url_attr
 
     @property
     def query_dict(self):
         if self._query_dict is None:
             self._query_dict = {}
 
-            for tmp_pair in self.url_tok.query.split("&"):
+            for tmp_pair in self.url_attr.query.split("&"):
                 key, value = get_split_tokens_from_line(tmp_pair, '=')
                 key = key.strip()
-                if not key: continue
+                if not key:
+                    continue
                 self._query_dict[key] = value.strip()
 
         return self._query_dict
 
     def get_hostname_token_list(self, is_normalize=False, is_prefix_value=False):
+        if self.url_attr.hostname is None:
+            raise UrlModuleError('The hostname of URL (%s) is None.' % self.url)
+
+        if self.is_ip():
+            return [self.url_attr.hostname]
+
         if is_normalize:
-            hostname = token_normalize(self.url_tok.hostname)
+            hostname = token_normalize(self.url_attr.hostname)
         else:
-            hostname = self.url_tok.hostname
+            hostname = self.url_attr.hostname
 
         if is_prefix_value:
             return [tok.strip()[:PREFIX_LENGTH] for tok in hostname.split(".") if tok.strip()]
@@ -50,21 +60,29 @@ class UrlModule(object):
 
     def get_path_token_list(self, is_normalize=False, append_file_extension=True):
         if is_normalize:
-            path = token_normalize(self.url_tok.path)
+            path = token_normalize(self.url_attr.path)
         else:
-            path = self.url_tok.path
+            path = self.url_attr.path
+
+        if not path.startswith('/'):
+            path = '/%s' % path
 
         path_tok = path.split("/")
-        last_tok = path_tok[-1].strip()
-        path_token = ['/%s/' % tok.strip() for tok in path_tok[:-1] if tok.strip()]
+        last_tok = path_tok[-1]
+        path_token = ['/%s/' % tok.strip() for tok in path_tok[1:-1] if tok.strip()]
+
+        if not last_tok:
+            return path_token
+
         if path.endswith('/'):
             path_token.append('/%s/' % last_tok)
-        elif last_tok:
+        else:
             path_token.append(last_tok)
 
             if append_file_extension:
                 filename, file_extension = get_last_dot_split_tuple(last_tok)
-                if file_extension: path_token.append(file_extension)
+                if file_extension:
+                    path_token.append(file_extension)
 
         return path_token
 
@@ -80,7 +98,7 @@ class UrlModule(object):
         return kv_list
 
     def is_ip(self):
-        return is_ip(self.url_tok.hostname)
+        return is_ip(self.url_attr.hostname)
 
 
 def get_last_dot_split_tuple(text):
@@ -129,6 +147,9 @@ def is_ipv6(_ip):
 
 
 def is_ip(addr):
+    if not addr:
+        return False
+
     netloc_toks = addr.split(":")
     len_netloc_toks = len(netloc_toks)
 
